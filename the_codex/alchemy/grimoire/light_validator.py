a535f2c1d45cafb80b4decd293e841498ed6e2b5
farahmkh21 <farahmkhald@gmail.com>
2026-08-23 13:03:30 +0300

    its just a code

--- the_codex/alchemy/grimoire/light_validator.py
+++
@@ -1,8 +0,0 @@
-def validate_ingredients(ingredients: str) -> str:
-
-    from .light_spellbook import (light_spell_allowed_ingredients)
-    allowed = (light_spell_allowed_ingredients())
-    for ingredient in allowed:
-        if ingredient.lower() in (ingredients.lower()):
-            return (f"{ingredients} - VALID")
-    return (f"{ingredients} - INVALID")
